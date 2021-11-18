a3169baa583e0f815d71d4a0704b37a782427615
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-11-18 13:57:08 +0000

    first product item

---
+++ src/Product.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Product({ id, title, price, rating, image }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {/* an empty array is created with Array, it is then filled with undefined values */}
+          {/* array is mapped and filled with stars chars */}
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
