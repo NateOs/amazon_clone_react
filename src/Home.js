019203c6934c74dfb12e300ff2265a568f35c995
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-11-25 11:14:19 +0000

    dummy data

--- src/Home.js
+++ src/Home.js
@@ -16,18 +16,45 @@ export default function Home() {
             className="home__image"
           />
           <div className="home__row">
-            <Product />
-            <Product />
-          </div>
-          <div className="home__row">
+            <Product
+              title="Duracell - CopperTop AA Alkaline Batteries - long lasting, all-purpose Double A battery for household and business - 24 Count
+              Single Use · 24 Count (Pack of 1)"
+              image="https://m.media-amazon.com/images/I/71634W89t5L._AC_UL320_.jpg"
+              rating={3}
+              price={29.99}
+            />
             <Product
               title="The lean startup"
               image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
               rating={5}
+              price={2.0}
+            />
+            <Product
+              title="Roku Streaming Stick+ | HD/4K/HDR Streaming Device with Long-range Wireless and Roku Voice Remote with TV"
+              image="https://m.media-amazon.com/images/I/71gBY-gGBwL._AC_UL320_.jpg"
+              rating={2}
+              price={25.99}
+            />
+          </div>
+          <div className="home__row">
+            <Product
+              title="Fire TV Stick 4K streaming device with Alexa Voice Remote (includes TV controls) | Dolby Vision"
+              image="https://m.media-amazon.com/images/I/51CgKGfMelL._AC_UL320_.jpg"
+              rating={4}
+              price={20.99}
+            />
+            <Product
+              title="Chuckit! Ultra Ball"
+              image="https://m.media-amazon.com/images/I/81an+TQ0ouL._AC_SY300_SX300_.jpg"
+              rating={5}
               price={29.99}
             />
-            <Product />
-            <Product />
+            <Product
+              title="Apple TV"
+              image="https://images-na.ssl-images-amazon.com/images/I/31ClCXhTfLL.jpg"
+              rating={4}
+              price={8.99}
+            />
           </div>
           <div className="home__row">{/*  */}</div>
         </div>
