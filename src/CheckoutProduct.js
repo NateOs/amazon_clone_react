09a6520367f6ec2df7740fffd275a83b2996e35c
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-11-27 20:49:59 +0000

    remove from basket

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ price, image, title, rating }) {
+  const [state, dispatch] = useStateValue();
+
+  const removeFromBasket = (id) => {
+    dispatch({ type: 'REMOVE_FROM_BASKET', id: id });
+  };
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="" />
@@ -18,7 +24,7 @@ function CheckoutProduct({ price, image, title, rating }) {
               <p>⭐</p>
             ))}
         </div>
-        <button onClick={() => removeFromBasket()}>Remove from basket</button>
+        <button onClick={() => removeFromBasket(1)}>Remove from basket</button>
       </div>
     </div>
   );
--- src/reducer.js
+++ src/reducer.js
@@ -12,6 +12,24 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_BASKET":
       return { ...state, basket: [...state.basket, action.item] };
+
+    case "REMOVE_FROM_BASKET":
+      const id = action.payload.id;
+      const currentBasket = [...state?.basket];
+      const newBasket = currentBasket.filter((item) => item.id !== id);
+      return { ...state, basket: newBasket };
+
+      // same implementation without id support
+      // const index = state.basket.findIndex(
+      //   (item) => item.id === action.payload.id
+      // );
+      // let newBasket = [...state.basket];
+      // if (index >= 0) {
+      //   newBasket.splice(index, 1);
+      // } else {
+      //   console.warn("item not found");
+      // }
+      // return { ...state, basket: newBasket };
     default:
       return state;
   }
