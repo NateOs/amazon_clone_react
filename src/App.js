c50fafd09530a9a04576cb04fb0a0112ab603b67
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-11-27 20:49:11 +0000

    new route added lesson learnt, add every R b4 root R

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
         <Switch>
           <Route path="/checkout">
             <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/">
             <Home />
