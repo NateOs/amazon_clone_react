de1202d5c88811722b9fe213d8c973aa8a5bbbb8
Nathan Sodja Obodai <sodjanathan@gmail.com>
2021-11-28 12:26:13 +0000

    finish login component

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+  const register = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
       <Link to="/login">
@@ -16,15 +25,30 @@ function Login() {
         <h1>Sign in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign In</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={(e) => signIn(e)}>
+            Sign In
+          </button>
           <p>
             By continuing, you agree to Amazon's Conditions of Use and Privacy
             Notice.
           </p>
-          <button className="login__registerButton">
+          <button
+            className="login__registerButton"
+            onClick={(e) => register(e)}>
             Create your Amazon Account
           </button>
         </form>
